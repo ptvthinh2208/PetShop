ccfd07f4b2b010bffd2b07b2fe84ba7d6d4300a2
LAPTOP-F571NC7F\ptvth <ptvthinh2208@gmail.com>
2023-12-29 18:35:13 +0700

    Build domain entities for project

---
+++ PetShop.Web/PetShop.Model/Models/Footer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PetShop.Model.Models
+{
+    public class Footer
+    {
+        [Key]
+        [MaxLength(50)]
+        public string ID { set; get; }
+
+        [Required]
+        public string Content { set; get; }
+    }
+}
---
+++ PetShop.Web/PetShop.Model/Models/Tag.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PetShop.Model.Models
+{
+    public class Tag
+    {
+        [Key]
+        [MaxLength(50)]
+        [Column(TypeName = "varchar")]
+        public string ID { set; get; }
+
+        [MaxLength(50)]
+        [Required]
+        public string Name { set; get; }
+
+        [MaxLength(50)]
+        [Required]
+        public string Type { set; get; }
+    }
+}
